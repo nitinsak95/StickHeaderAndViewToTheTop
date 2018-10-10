94fce31c68a3c9aabee26b240ead4b473cbbe093
affixusimac1 <viveking@outlook.com>
2018-10-10 11:02:18 +0530

    1st commit

--- stickyHeaderScrollView/ViewController.swift
+++ stickyHeaderScrollView/ViewController.swift
@@ -8,18 +8,45 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIScrollViewDelegate, UITextViewDelegate {
 
+    @IBOutlet weak var topView: UIView!
+    @IBOutlet weak var stickView: UIView!
+    @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet weak var textView: UITextView!
+    var navigationBarOriginalOffset : CGFloat?
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        textView.delegate = self
+        scrollView.delegate = self
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationBarOriginalOffset = stickView.frame.origin.y
+    }
 
-
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        stickView.frame.origin.y = max(navigationBarOriginalOffset!, scrollView.contentOffset.y)
+    }
+    
+    func textViewDidChange(_ textView: UITextView) {
+        let fixedWidth = textView.frame.size.width
+        textView.sizeThatFits(CGSize(width: fixedWidth, height: CGFloat.greatestFiniteMagnitude))
+        let newSize = textView.sizeThatFits(CGSize(width: fixedWidth, height: CGFloat.greatestFiniteMagnitude))
+        var newFrame = textView.frame
+        newFrame.size = CGSize(width: max(newSize.width, fixedWidth), height: newSize.height)
+        textView.frame = newFrame
+    }
+    
 }
 
